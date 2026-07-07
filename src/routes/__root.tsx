d20e977260c96aa93ce6964a0e8d07bab0b5e1d4
ymtdir <ymtdir0407@gmail.com>
2026-07-08 08:53:06 +0900

    style: favicon link タグを head に追加

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,6 +23,11 @@ export const Route = createRootRoute({
         rel: "stylesheet",
         href: appCss,
       },
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+        type: "image/x-icon",
+      },
     ],
   }),
   notFoundComponent: () => (
